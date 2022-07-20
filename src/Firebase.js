855a7042eb3911bed00aa38921a86116800d2790
agiledev0115 <alexanderagiledev@gmail.com>
2022-07-20 12:12:20 +0900

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,20 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-// Documentation
-// https://firebase.google.com/docs/auth/web/google-signin
-
 // Note: "etsuji-react-test" is a non-existent project.
-const firebaseConfig = {
-  apiKey: "AIzaSyBK_Tb1FGifScE6a1ZVEOg0AX-_0HMFdH8",
-  authDomain: "etsuji-mab-demo.firebaseapp.com",
-  projectId: "etsuji-mab-demo",
-  storageBucket: "etsuji-mab-demo.appspot.com",
-  messagingSenderId: "108019528439",
-  appId: "1:108019528439:web:2cb648130dc2dec438d9f2"
-};
-
-/*
 const firebaseConfig = {
   apiKey: "AIzaSyDRzeNQHTP5KAxHHtlcPzHN2PD_ZLm4a5M",
   authDomain: "etsuji-react-test.firebaseapp.com",
@@ -23,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "816169103559",
   appId: "1:816169103559:web:41b4b597600bc9bea64ee2"
 };
-*/
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
